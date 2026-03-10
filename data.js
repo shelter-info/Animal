d76095b2dffd526c67492970c07f4f1f8ec12421
Justin <justin.redmayne75@gmail.com>
2026-03-11 00:09:45 +0900

    Update data.js

--- data.js
+++ data.js
@@ -13,86 +13,86 @@ const shelters = [
     type: "petshop",
     instagram: "https://www.instagram.com/dogmaru.shelter/",
     address: "전국 모든 지점",
-    "homepage": "https://dmanimal.co.kr/",
-    "business": "3560000-045-2024-0002"
+    homepage: "https://dmanimal.co.kr/",
+    business: "3560000-045-2024-0002"
   },
   {
-    "name": "아이조아",
+    name: "아이조아",
     type: "petshop",
-    "instagram": "https://instagram.com/ijoahappy",
-    "address": "서울시 마포구 독막로28길 10",
-    "homepage": "https://www.ijoa.co.kr/",
-    "business": "3130000-045-2025-0001"
+    instagram: "https://instagram.com/ijoahappy",
+    address: "서울시 마포구 독막로28길 10",
+    homepage: "https://www.ijoa.co.kr/",
+    business: "3130000-045-2025-0001"
   },
   {
-    "name": "따숨분양센터",
+    name: "따숨분양센터",
     type: "petshop",
-    "instagram": "https://instagram.com/ttasum_catdog",
-    "address": "-,
-    "homepage": "-",
-    "business": "554000004820260003"
+    instagram: "https://instagram.com/ttasum_catdog",
+    address: "-",
+    homepage: "-",
+    business: "554000004820260003"
   },
   {
-    "name": "다온입양센터",
+    name: "다온입양센터",
     type: "petshop",
-    "instagram": "https://instagram.com/daon__center",
-    "address": "경기도 남양주시 와부읍 안골로 49-9 3",
-    "homepage": "https://daon-center.com/",
-    "business": "3990000-045-2022-0035"
+    instagram: "https://instagram.com/daon__center",
+    address: "경기도 남양주시 와부읍 안골로 49-9 3",
+    homepage: "https://daon-center.com/",
+    business: "3990000-045-2022-0035"
   },
   {
-    "name": "강아지고양이입양센터",
+    name: "강아지고양이입양센터",
     type: "petshop",
-    "instagram": "https://instagram.com/safe__paw__",
-    "address": "인천광역시 서구 청라 커낼로260번길 27",
-    "homepage": "https://safepawcenter.com/",
-    "business": "356000-045-2024-0004"
+    instagram: "https://instagram.com/safe__paw__",
+    address: "인천광역시 서구 청라 커낼로260번길 27",
+    homepage: "https://safepawcenter.com/",
+    business: "356000-045-2024-0004"
   },
-   {
-    "name": "안산동물보육원",
+  {
+    name: "안산동물보육원",
     type: "petshop",
-    "instagram": "https://instagram.com/sa_rang0222",
-    "address": "경기도 안산시 시낭로2길 1 2층",
-    "homepage": "https://www.asanimal.com/",
-    "business": "670-20-01640"
+    instagram: "https://instagram.com/sa_rang0222",
+    address: "경기도 안산시 시낭로2길 1 2층",
+    homepage: "https://www.asanimal.com/",
+    business: "670-20-01640"
   },
 
-  // 실제 보호소 예시 ---------------------------------------------------------------------------------
- {
-    "name": "마이독모먼트",
-    "type": "shelter",
-    "instagram": "https://instagram.com/my_dog_moment_2024",
-    "address": "충남 공주시"
+  // 실제 보호소 --------------------------------------------------------------------------------------------------------------------------
+  {
+    name: "마이독모먼트",
+    type: "shelter",
+    instagram: "https://instagram.com/my_dog_moment_2024",
+    address: "충남 공주시"
   },
   {
-    "name": "쏘바이",
-    "type": "shelter",
-    "instagram": "https://instagram.com/soby2017",
-    "address": "-"
+    name: "쏘바이",
+    type: "shelter",
+    instagram: "https://instagram.com/soby2017",
+    address: "-"
   },
   {
-    "name": "리패밀리",
-    "type": "shelter",
-    "instagram": "https://instagram.com/re_family_rescue",
-    "address": "-"
+    name: "리패밀리",
+    type: "shelter",
+    instagram: "https://instagram.com/re_family_rescue",
+    address: "-"
   },
   {
-    "name": "도로시지켜줄개",
-    "type": "shelter",
-    "instagram": "https://instagram.com/dorothyrescue",
-    "address": "-"
+    name: "도로시지켜줄개",
+    type: "shelter",
+    instagram: "https://instagram.com/dorothyrescue",
+    address: "-"
   },
   {
-    "name": "엔젤프로젝트",
-    "type": "shelter",
-    "instagram": "https://instagram.com/angelproject_official",
-    "address": "-"
+    name: "엔젤프로젝트",
+    type: "shelter",
+    instagram: "https://instagram.com/angelproject_official",
+    address: "-"
   },
   {
-    "name": "네스트",
-    "type": "shelter",
-    "instagram": "https://instagram.com/nest_animal_2011",
-    "address": "-"
-  },
+    name: "네스트",
+    type: "shelter",
+    instagram: "https://instagram.com/nest_animal_2011",
+    address: "-"
+  }
 
 ];
